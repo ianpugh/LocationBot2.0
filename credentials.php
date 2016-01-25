d110a09ad8514fb7c7df527ce1d5ca66695d995e
ianpugh <ianpugh@gmail.com>
2016-01-25 07:55:45 -0600

    Create credentials.php

---
+++ credentials.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_HOST', ''); // mysql host name
+define('DB_USERNAME', ''); // mysql username
+define('DB_PASSWORD', ''); // mysql password
+define('DB_NAME', ''); // mysql database name
+
+define('REDDIT_USERNAME', ''); // reddit username
+define('REDDIT_PASSWORD', ''); // reddit password
+?>
