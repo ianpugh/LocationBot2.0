932e02aa84754c95c0dbd997b3710b3d2be2fea8
ianpugh <ianpugh@gmail.com>
2016-03-10 13:49:36 -0600

    Added functionality for mod queue monitoring.

--- monitor_action.php
+++ monitor_action.php
@@ -1,3 +1,73 @@
 <?php
 
+require_once('tracking.php');
+
+class MentionAction
+{
+	public $thing_id;
+	
+	public $match_collection = array();
+	
+	private $reddit;
+	
+	private $subreddit;
+	
+	function  __construct ($post, $reddit, $db, $subreddit_rec_id)
+	{
+		$this->thing_id = $db->real_escape_string($post->data->name);
+		$this->reddit = $reddit;
+		$this->db = $db;
+		$thid->subreddit = $subreddit;
+	}
+	
+	private function CurlRequest($url)
+	{
+		$ch = curl_init();
+		
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
+		curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: ' . USER_AGENT);
+		
+		$result = curl_exec($ch);
+		curl_close($ch);
+		
+		return (array) json_decode($result);
+	}
+	
+	private function GetReportsLog($subreddit)
+	{
+		$url = "https://www.reddit.com/r/$subreddit/about/reports/.json";
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
+		curl_setopt($ch, CURLOPT_USERAGENT, USER_AGENT);
+		
+		$result = curl_exec($ch);
+		curl_close($ch);
+		return (array) json_decode($result);
+	}
+	
+	public function PerformAction()
+	{
+		$moderator_list = array();
+		
+		// Do we have any info in the modlog?
+		$reports = $this->GetReportsLog($subreddit)
+		
+		foreach ($reports[data]->children as $report)
+		{
+			$author = $report->author;
+			$url = $report->url;
+			$reports = array();
+			
+			foreach ($report->data->user_reports as $reason)
+			{
+				$query = "INSERT INTO PostReport (link_id, link_author, reason, ,url, body) VALUES ('{$report->data->name}','{$report->data->link_author}','{$reason}', '{$report->data->link_url)";
+				$this->db->query($query);
+			}
+		}
+	}
+}
 ?>
